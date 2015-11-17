b6d09b4b7f40acf1fe4a61d865bcb42ad96122ee
Matt Bittarelli <mbittarelli@Matts-MacBook-Pro.local>
2015-11-17 17:14:23 -0500

    updating all the uuids for the flash types and all the global defines for the plugin

--- SampleCreateJSPlatform/Plugin/SampleCreateJSPlugin/include/PluginConfiguration.h
+++ SampleCreateJSPlatform/Plugin/SampleCreateJSPlugin/include/PluginConfiguration.h
@@ -26,14 +26,14 @@
 #ifndef _PLUGIN_CONFIGURATION_H_
 #define _PLUGIN_CONFIGURATION_H_
 
-#define PUBLISHER_NAME						"SamplePlugin"
-#define PUBLISHER_UNIVERSAL_NAME			"com.example.SamplePluginPublisher"
+#define PUBLISHER_NAME						"Jibo"
+#define PUBLISHER_UNIVERSAL_NAME			"JiboPublisher"
 
 /* The value of the PUBLISH_SETTINGS_UI_ID has to be the HTML extension ID used for Publish settings dialog*/
-#define PUBLISH_SETTINGS_UI_ID				"com.example.SamplePlugin.PublishSettings"
+#define PUBLISH_SETTINGS_UI_ID				"JiboPixiJSPlugin.PublishSettings"
 
-#define DOCTYPE_NAME						"SamplePlugin"
-#define DOCTYPE_UNIVERSAL_NAME				"com.example.SamplePlugin"
+#define DOCTYPE_NAME						"JiboPixiJSPlugin"
+#define DOCTYPE_UNIVERSAL_NAME				"JiboPixiJSPlugin"
 #define DOCTYPE_DESCRIPTION					"This document can be used to author content for sample runtime"
 
 /* The value of RUNTIME_FOLDER_NAME must be the name of the runtime folder present in EclipseProject/ExtensionContent. */
@@ -41,30 +41,29 @@
 
 namespace CreateJS
 {
-    // {835B2A74-9646-43AD-AA86-A35F4E0ECD1B}
+    // {d30f8a0d-8234-44e1-9e30-ee48b415be9d}
     const FCM::FCMCLSID CLSID_DocType =
-        {0x835b2a74, 0x9646, 0x43ad, {0xaa, 0x86, 0xa3, 0x5f, 0x4e, 0xe, 0xcd, 0x1b}};
+        {0xd30f8a0d, 0x8234, 0x44e1, {0x9e, 0x30, 0xee, 0x48, 0xb4, 0x15, 0xbe, 0x9d}};
 
-    // {50705258-F3B8-4CFF-929B-E10EC7DA8816}
+    // {afeccca4-7b79-47e4-aa19-d5e646d47fbe}
     const FCM::FCMCLSID CLSID_FeatureMatrix =
-        {0x50705258, 0xf3b8, 0x4cff, {0x92, 0x9b, 0xe1, 0xe, 0xc7, 0xda, 0x88, 0x16}};
+        {0xafeccca4, 0x7b79, 0x47e4, {0xaa, 0x19, 0xd5, 0xe6, 0x46, 0xd4, 0x7f, 0xbe}};
 
-	    // {1635256D-2F63-4715-BC70-6B2948CC84D5}
+	    // {8f6f8054-505f-472a-b602-f78cfc350563}
     const FCM::FCMCLSID CLSID_Publisher =
-        {0x1635256d, 0x2f63, 0x4715, {0xbc, 0x70, 0x6b, 0x29, 0x48, 0xcc, 0x84, 0xd5}};
+        {0x8f6f8054, 0x505f, 0x472a, {0xb6, 0x02, 0xf7, 0x8c, 0xfc, 0x35, 0x05, 0x63}};
 
-
-    // {D5830903-02D6-4133-A1F1-F272D29A1802}
+    // {c19ef743-6c1a-4076-9ab2-d486a94f0393}
     const FCM::FCMCLSID CLSID_ResourcePalette =
-        {0xd5830903, 0x2d6, 0x4133, {0xa1, 0xf1, 0xf2, 0x72, 0xd2, 0x9a, 0x18, 0x2}};
+        {0xc19ef743, 0x6c1a, 0x4076, {0x9a, 0xb2, 0xd4, 0x86, 0xa9, 0x4f, 0x03, 0x93}};
 
-    // {70E17C74-34DF-4C46-8F9D-900D2EC045E3}
+    // {95c69e19-de0f-4c8e-8388-e7d4d8db98c0}
     const FCM::FCMCLSID CLSID_TimelineBuilder =
-        {0x70e17c74, 0x34df, 0x4c46, {0x8f, 0x9d, 0x90, 0xd, 0x2e, 0xc0, 0x45, 0xe3}};
+        {0x95c69e19, 0xde0f, 0x4c8e, {0x83, 0x88, 0xe7, 0xd4, 0xd8, 0xdb, 0x98, 0xc0}};
 
-    // {442305C9-F02F-43A4-B259-D1CC39666A94}
+    // {e31e2a88-f57e-4106-87dc-511623b10fb8}
     const FCM::FCMCLSID CLSID_TimelineBuilderFactory =
-        {0x442305c9, 0xf02f, 0x43a4, {0xb2, 0x59, 0xd1, 0xcc, 0x39, 0x66, 0x6a, 0x94}};
+        {0xe31e2a88, 0xf57e, 0x4106, {0x87, 0xdc, 0x51, 0x16, 0x23, 0xb1, 0x0f, 0xb8}};
 }
 
 
