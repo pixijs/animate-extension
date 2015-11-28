77c7811cf4fb9398f79269274096aedda0d905d9
Matt Bittarelli <matt.bittarelli@jibo.com>
2015-11-27 20:56:36 -0500

    comments

--- SampleCreateJSPlatform/Utils/install_plugin.sh
+++ SampleCreateJSPlatform/Utils/install_plugin.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# copy the build plugin from the xcode directory to the eclipse directory
+
 BUILD_TARGET=$1;
 BUILD_SCHEME="";
 
+# verify the command line args
 if [ ${BUILD_TARGET} == "debug" ]; then
     BUILD_SCHEME="Debug";
 elif [ ${BUILD_TARGET} == "release" ]; then
@@ -12,13 +13,16 @@ else
     exit 1;
 fi
 
+# build the xcode project to produce the plugin file
 xcodebuild -project ../Plugin/SampleCreateJSPlugin/project/mac/SampleCreateJSPlugin.mp.xcodeproj -scheme SampleCreateJSPlugin.${BUILD_SCHEME} build
 
 PLUGIN_FILE=../Plugin/SampleCreateJSPlugin/lib/mac/${BUILD_TARGET}/JiboPixiJSPlugin.fcm.plugin
 
+# the plugin file needs to be placed in the eclipse project directory so it can be exported to a zxp
 echo "Copy the plugin to the Eclipse Project directory";
 cp -r ${PLUGIN_FILE} ../EclipseProject/ExtensionContent/plugin/lib/mac/JiboPixiJSPlugin.fcm.plugin
 
+# make sure the previous runs output has been cleared from the adobe flash extensions directory
 echo "Deleting the previously created zxp file and the unpacked contents";
 rm -rf /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin/*
 
@@ -36,5 +40,3 @@ mv /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin/.staged-e
 mv /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin/ExtensionContent/* /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin 
 
 exit 0;
-
-tar -xzvf /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin/plugin_contents/EclipseProject.zxp -C /Library/Application\ Support/Adobe/CEP/extensions/JiboPixiJSPlugin
