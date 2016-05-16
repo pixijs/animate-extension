63a07981b104eedd1e08d367d0678fe935a88848
Matt Karl <matt.karl@jibo.com>
2016-05-16 14:37:54 -0400

    1.0.25

--- src/PixiAnimate/include/Version.h
+++ src/PixiAnimate/include/Version.h
@@ -56,7 +56,7 @@
      *
      * @brief Plugin version's maintenance number. 
      */
-    #define PIXIJS_PLUGIN_VERSION_MAINTENANCE           0x18
+    #define PIXIJS_PLUGIN_VERSION_MAINTENANCE           0x19
     
 
     /**
