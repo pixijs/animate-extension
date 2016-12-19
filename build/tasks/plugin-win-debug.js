c0f71454b28937c4645de50be5c3db2a393ed29b
Matt Karl <matt.karl@jibo.com>
2016-12-19 10:14:52 -0800

    Adds environmental variales for VC

--- build/tasks/plugin-win-debug.js
+++ build/tasks/plugin-win-debug.js
@@ -1,5 +1,6 @@
 module.exports = function(gulp, options, plugins) {
     gulp.task('plugin-win-debug', function() {
+        process.env.VCTargetsPath = options.VCTargetsPath;
         return gulp.src(options.projectFile)
             .pipe(plugins.msbuild({
                 targets: ['Clean', 'Build'],
