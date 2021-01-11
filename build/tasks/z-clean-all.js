ff991873c8876eb6b7dbd6dc8c5c9479195426ea
andrewstart <start.andrew@gmail.com>
2021-01-10 23:37:52 -0500

    Fix clean-all build task.

--- build/tasks/z-clean-all.js
+++ build/tasks/z-clean-all.js
@@ -1,5 +1,3 @@
 module.exports = function(gulp, options, plugins) {
-    gulp.task('clean-all', function(done) {
-        gulp.series('clean', 'clean-build', 'uninstall', done);
-    });
+    gulp.task('clean-all', gulp.series('clean', 'clean-build', 'uninstall'));
 };
